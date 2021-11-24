cfc4c26062bf48b3c2219e14b754c781ec9c877e
LibniHuaranga <73641532@ucsm.edu.pe>
2021-11-23 23:43:38 -0500

    Se subieron los dos ejercicios de lla practica, uno con la creacion de ASP.net Core y el otro con google OAuth

---
+++ Ejercicio2--11/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicio2__11.MvcApplication" Language="C#" %>
---
+++ Ejercicio2--11/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ejercicio2__11.Startup))]
+namespace Ejercicio2__11
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
